90fb5febeba55d552be2aeaa0df98a9d0dd1cc93
Mille af Rolén <milleafrolen@n164-p71.eduroam.kth.se>
2023-10-09 15:11:18 +0200

    Finished with assignment 3

--- p3.m
+++ p3.m
@@ -3,7 +3,15 @@ load wave_data.mat
 subplot(2,1,1), plot(y(1:end))
 subplot(2,1,2), hist_density(y)
 
-hold on     % Gor sa att ploten halls kvar
+ybar = mean(y);
+alpha = 0.05;
+my_est = ybar/sqrt(pi/2);
+D = sqrt((2 * ybar.^2 * (4 - pi) / (pi.^2 * M)));
+critical_value = norminv(1 - alpha/2);
+lower_bound = my_est - D * critical_value;
+upper_bound = my_est + D * critical_value;
+
+hold on % Gor sa att ploten halls kvar
 plot(lower_bound, 0, 'g*')
 plot(upper_bound, 0, 'g*')
 
